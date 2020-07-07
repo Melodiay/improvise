27e5c36aff36e7165f567a1f70f71c9afd7c2d04
Alexey <58159173+Melodiay@users.noreply.github.com>
2020-07-07 13:00:24 +0300

    Add files via upload

---
+++ workflows/MyPrograms_C/62/Windows/winlotto.c
@@ -0,0 +1,53 @@
+// gcc winlotto.c -o winlotto.exe 
+// winlotto.exe
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <errno.h>
+#include <windows.h> 
+
+extern int errno;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+
+{
+	
+    int s;
+
+    int i, r, temp, nums[50];
+
+    char buf[4], str[50] = { "Your Six Lucky Numbers Are: " };
+
+    srand(time(NULL));
+
+    for(i = 0; i < 50; i++) { nums[i] = i; }
+
+    for(i = 1; i < 50; i++)
+
+    {
+
+	r = (rand() % 49 ) + 1;
+
+	temp = nums[i]; nums[i] = nums[r]; nums[r] = temp;
+
+    }
+
+    for(i = 1; i < 7; i++)
+
+    {
+
+	sprintf(buf, "%d", nums[i]);
+
+	strcat(buf, " "); strcat(str, buf);
+
+    }
+	
+	MessageBox(NULL, str, "Lotto Number Picker", MB_OK | MB_ICONEXCLAMATION);
+
+  return 0; 
+} 
+
+
+
